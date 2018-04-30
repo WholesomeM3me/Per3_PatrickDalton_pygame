54c9b520de46dd757618c5ec7d820d968ee0d1a1
Patrick Dalton <PDalton8385@student.pps.net>
2018-04-30 13:22:59 -0700

    Reworded questions

--- game_window.py
+++ game_window.py
@@ -1,7 +1,7 @@
 import pygame
 
-x = int(input("How wide do you want the window resolution to be in pixels?"))
-y = int(input("How tall do you want the window resolution to be?"))
+x = int(input("How many pixels wide do you want the window resolution to be?"))
+y = int(input("How many pixels tall do you want the window resolution to be?"))
 screen = pygame.display.set_mode((x, y))
 running = True
 
