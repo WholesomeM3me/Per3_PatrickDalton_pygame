7d5c1616e78c6bee3d424682f484169db8ed3301
Patrick <patrickdalton811@gmail.com>
2018-05-10 18:59:37 +0000

    Fixed issues

--- gpio_control.py
+++ gpio_control.py
@@ -4,7 +4,7 @@ from time import sleep
 led = LED(17)
 button = Button(2)
 
-button.wait_for_press()
-led.on()
-sleep(3)
-led.off()
+while True:
+    button.wait_for_press()
+    led.toggle()
+    sleep(0.5)
--- led.py
+++ led.py
@@ -1,12 +1,18 @@
 from gpiozero import LED
 from time import sleep
-
 led = LED(17)
 
-while True:
-    led.on()
-    sleep(1)
-    led.off()
-    sleep(1)
+def LED_blink_time(led_on, led_off):
+    """Defines length of blinks and time betweem blinks"""
+    while True:
+        led.on()
+        sleep(led_on)
+        led.off()
+        sleep(led_off)
+
+led_on= float(input("How long should the LED stay on?"))
+led_off= float(input("How long sohuld the LED stay off?"))
+
+LED_blink_time(led_on, led_off)
 
 
